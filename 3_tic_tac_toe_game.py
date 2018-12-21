da8db6cdff877f28d028c7f8035af54f08b69d3d
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2018-12-21 12:07:00 +0530

    added list of game and printing it

---
+++ 3_tic_tac_toe_game.py
@@ -0,0 +1,13 @@
+game = [
+		[0,0,0],
+		[0,0,0],
+		[0,0,0],
+	   ]
+
+print("class of game is " , type(game))
+
+print("\ngame list-datatype is ", game)
+
+print("\nrow is ")
+for row in game:
+	print(row)
