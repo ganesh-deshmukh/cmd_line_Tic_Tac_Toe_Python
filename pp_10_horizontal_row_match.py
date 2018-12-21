941154adb73bdb1ed919c563003b8e2a3b363cea
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2018-12-22 00:23:12 +0530

    checking horizonatal winner from matching all elements of rows

---
+++ pp_10_horizontal_row_match.py
@@ -0,0 +1,26 @@
+# DON'T Trust user's Input
+game = [
+		[1,0,1],
+		[0,1,0],
+		[1,1,1],
+	   ]
+# You can win Tic-Tac-Toe game by 4 ways
+# 1) Horizontally
+# 2) Vertically
+# 3) Diagonally 
+# 4) Inverse-Diagonally
+
+def win(current_game):
+	for row in game:
+		print("for row =", row)
+		all_match = True
+		for item in row:
+			# print("if( " + item + " !== " + row[0] + " )")
+			print(item,row)
+			if item != row[0]:
+				all_match= False
+
+		if all_match:
+			print("Winner")
+
+win(game)
