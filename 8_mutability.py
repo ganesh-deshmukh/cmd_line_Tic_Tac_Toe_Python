32f0b7420073f2044aa6c0067896e318ee34ac80
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2018-12-21 19:56:08 +0530

    changing value by keyward 'global'

--- 8_mutability.py
+++ 8_mutability.py
@@ -1,11 +1,12 @@
-game = [1,2,3]
+game = "I wanted to play this Game now"
 print(id(game))
 
-def gaming_params(input=0, row=0, col=0, just_display=False):
-	game[0] = 100 
-	print(id(game))
 
-	print("Local scope value  ", game)  
+def gaming_params(input=0, row=0, col=0, just_display=False):
+	global game
+	game = "A small Game"
+	print(id(game))
+	print(game)  
 
 	# print("     0  1  2")
 	# if not just_display:
@@ -13,9 +14,9 @@ def gaming_params(input=0, row=0, col=0, just_display=False):
 	# for count, row in enumerate(game):
 	# 	print(count, row)
 
-print("Before calling func ", game)
+print(game)
 print(id(game))
 gaming_params() # this will be executed according to sequence.
-print("after calling func ", game)
+print(game)
 print(id(game))
 
