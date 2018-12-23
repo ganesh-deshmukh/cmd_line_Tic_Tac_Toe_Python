470bed1b100f7634525dfdc184783fbe135b5332
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2018-12-23 21:23:23 +0530

    changed default running env of python to py3 in sublimetext3

--- 9_error_handling.py
+++ 9_error_handling.py
@@ -15,6 +15,7 @@ def gaming_params(game_map, input=0, row=0, col=0, just_display=False):
 			print(index,row)
 
 		return game_map
+
 	except IndexError as e:
 		print("You have entered value of row/column other than 0,1,2\n",e)
 		# print("you have an error, please follow syntax and check logic")
@@ -22,5 +23,7 @@ def gaming_params(game_map, input=0, row=0, col=0, just_display=False):
 		print("Error is not caught, so General Exception is called!\n",e)
 	finally :
 		print("It will run compulsorily")
+
+
 game = gaming_params(game, just_display=True)
-game = gaming_params(gaming_params, input=1, row=3, col=1) 
+game = gaming_params(game, input=2, row=1, col=1) 
--- pp_13_iterators_iterables.py
+++ pp_13_iterators_iterables.py
@@ -1,50 +1,43 @@
 game = [
-        [1,2,1],
-        [1,0,0],
-        [1,2,1],
-       ]
+		[1,2,0],
+		[0,2,1],
+		[1,2,1],
+	   ]
 
 # len(game) = 3
 
 def win(current_game):
-    #Horizontal
-    for row in game:
-        print(row)
-        if row.count(row[0]) == len(row) and row[0] != 0 :
-            print("Horizontally")
-            # print(f"Player {row[0]} Won Horizontally ")
+	#Horizontal
+	for row in game:
+		print(row)
+		if row.count(row[0]) == len(row) and row[0] != 0 :
+			print("Horizontally")
+			print(f"Player {row[0]} Won Horizontally ")
 
-    # Diagonal
-    diags = [] 
-    for col,row in enumerate(reversed(range(len(game)))): # It started from index=2 upto index=0
-        diags.append(game[row][col])
-    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+	# Diagonal
+	diags = [] 
+	for col,row in enumerate(reversed(range(len(game)))): # It started from index=2 upto index=0
+		diags.append(game[row][col])
+	if diags.count(diags[0]) == len(diags) and diags[0] != 0:
 
-        print("Diagonally (/) ")
-        # print(f"Player {diags[0]} Reverse-Diagonally (/)")
+		print("Diagonally (/) ")
+		print(f"Player {diags[0]} Reverse-Diagonally (/)")
 
-    diags = []
-    for ix in range(len(game)):
-        diags.append(game[ix][ix])
-    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
-        print("Diagonally (\\)")
-        # print(f"Player {diags[0]} Diagonally (\\)")
+	diags = []
+	for ix in range(len(game)):
+		diags.append(game[ix][ix])
+	if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+		print("Diagonally (\\)")
+		print(f"Player {diags[0]} Diagonally (\\)")
 
-    #Vertical
-    for col in range(len(game)):
-        check  = [] 
-        for row in game:
-            check.append(row[col])
+	#Vertical
+	for col in range(len(game)):
+		check  = [] 
+		for row in game:
+			check.append(row[col])
 
-        if check.count(check[0]) == len(check) and check[0] != 0:
-            print("Vertically")
-            # print(f"Player {row[0]} Vertically (|)")
-
+		if check.count(check[0]) == len(check) and check[0] != 0:
+			print("Vertically")
+			print(f"Player {check[0]} Vertically =")
 
 win(game)
-
-# # print("game is = ", game)
-# for index, row in game:
-#   print()
-# print("diags is  ", diags)
-# print("reversed(range(len(game))) ",reversed(range(len(game))))
